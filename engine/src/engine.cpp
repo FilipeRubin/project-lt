119cc37108141472fd7f53b3c3f1c2cfe7c076b6
Rubin <filiperubin@gmail.com>
2024-09-01 00:39:07 -0300

    Configure further and add boilerplate code

---
+++ engine/src/engine.cpp
@@ -0,0 +1,7 @@
+#include "engine.h"
+#include <iostream>
+
+void Engine::Run()
+{
+	std::cout << "The engine is running!\n";
+}
---
+++ game/main.cpp
@@ -0,0 +1,9 @@
+#include <engine.h>
+
+int main()
+{
+	Engine engine = Engine();
+	engine.Run();
+
+	return 0;
+}
